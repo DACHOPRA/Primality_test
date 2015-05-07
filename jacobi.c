db2224a6fc48edfbf0a91291e55b646c17317835
sionar <eterna@gmail.com>
2015-05-07 05:16:11 -0400

    added lucas to jacobi

--- jacobi.c
+++ jacobi.c
@@ -7,15 +7,32 @@
 
 int main(int argc, char *argv[])
 {
-  int jacobi(unsigned long long int d, unsigned long long int n);
+	int jacobi(unsigned long long int d, unsigned long long int n);
+	int lucasPrime (unsigned long long int n, unsigned long long int d);
 
-  unsigned long long int d, n;
-  int result;
+	unsigned long long int d, n, p, dAbs, sign;
+	int result;
+	
+	
 
-  d = 111;
-  n = 3337;
-  result = jacobi(d,n);
-  printf("d %lld, n %lld, result %d \n",d,n,result);
+	n = 117;
+	d = 5;
+	dAbs = 5;
+	sign = 1;
+	result = 0;
+/*
+	while (result != -1)
+	{
+		d = dAbs*sign;	
+		result = jacobi(d,n);
+		dAbs = dAbs + 2;
+		sign = sign * -1;
+	}
+*/
+//	printf("d %lld, n %lld, result %d \n",d,n,result);
+	result = lucasPrime(d,n);
+	printf("d %lld, n %lld, result %d \n",d,n,result);
+	
 }
 
 
@@ -41,4 +58,59 @@ while (d != 0)
 }
 if (n == 1) return t;
 return 0;
+}
+
+int lucasPrime (unsigned long long int d, unsigned long long int n)
+{   
+	int p, i, length,k;
+	unsigned long long int q, q2, u, u2, uold, v, v2, t;
+
+//	if (gcd(d,n) != 1)
+//		return (0);
+
+	p = 1;
+	q = (1 - d)/4;
+	u = 0;
+	v = 2;
+	u2 = 1;
+	v2 = p;
+	q2 = 2*q;
+
+	t = (n+1)/2;						//theta
+	length = sizeof(unsigned long long int)*8 - __builtin_clzll(t); //length of our number in bits. //clz(b00010010) = 3 	
+//	printf("length = %d\n", length);	
+
+	for (i = 0; i < length-1; i++)
+	{	
+		u2 = (u2 * v2) % n;
+		v2 = (v2 * v2 - q2) % n;
+		if (t & 1)				//mask = 1
+		{
+			uold = u;
+			u = (u2 * v) + (u * v2);
+			if (u % 2 == 1 ) u = u+n;
+			u = (u / 2) % n;
+			v = (v2 * v) + (u2 * uold * d);
+			if (v % 2 == 1 ) v = v+n;  
+			v = (v / 2) % n;
+		}
+
+		q = (q*q) % n;
+		q2 = q + q;
+
+		t = t >> 1;
+		printf("u = %lld\n", u);	
+	}
+	return (u == 0);
+}
+
+int gcd (unsigned long long int a, unsigned long long int b)
+{
+  unsigned long long int c;
+  while (a != 0) {
+     c = a; 
+	 a = b%a;  
+	 b = c;
+  }
+  return b;
 }
